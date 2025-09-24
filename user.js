0ca303c9ff8475483b850fd5a209e7b7034deae5
SolarDaz3 <jeremygalan@hotmail.com>
2025-09-24 11:44:23 -0400

    Broken js?

---
+++ user.js
@@ -0,0 +1,22 @@
+const postListEl = document.querySelector(".post-list")
+
+async function main() {
+     const id = localStorage.getItem("id")
+ const posts = await fetch (`https:jsonplaceholder.typicode.com/posts?userId=:${id}`)
+  const postsData = await posts.json();
+  console.log(postsData)
+
+
+  postListEl.innerHTML = postsData.map(post => 
+      `<div class="${post.id}">
+      <div class="post__title">
+      Post Title
+      </div>
+      <p class="post__body">
+      Post Body
+      </p>
+      </div>`).join('');
+
+ }
+
+ main();
