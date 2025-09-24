a6a2e87d2f6f40015ff04703358ca72ec211f804
SolarDaz3 <jeremygalan@hotmail.com>
2025-09-24 13:18:16 -0400

    complete

--- user.js
+++ user.js
@@ -1,14 +1,34 @@
-const postListEl = document.querySelector(".post-list")
+const postListEl = document.querySelector('.post-list');
+const id = localStorage.getItem("id")
 
-async function main() {
-     const id = localStorage.getItem("id")
- const posts = await fetch (`https://jsonplaceholder.typicode.com/posts?userId=:${id}`)
-  const postsData = await posts.json();
-  console.log(postsData)
-
+async function onSearchChange(event) {
+    const id = event.target.value
+renderPosts(id)
 
 
+ }   
 
- }
+async function renderPosts(id) {
+    const posts = await fetch (`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+     const postsData = await posts.json();
+       postListEl.innerHTML = postsData.map(post => postsHTML(post)).join('');
+   
+   
+    }
+   
+    function postsHTML(post) {
+       return `
+           <div class="post">
+         <div class="post__title">
+         ${post.title}
+         </div>
+         <p class="post__body">
+         ${post.body}
+         </p>
+         </div>
+     </div>
+     `
+    } 
+     
 
- main();
+ renderPosts(id);
